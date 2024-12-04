7288f8016716a5e3f766b04a4c72a58ce53e17d0
فاطمه یافته <fatemmehyafteh@gmail.com>
2024-12-04 16:40:54 +0300

    emp

---
+++ src/components/Province.js
@@ -0,0 +1,22 @@
+// import axios from 'axios'
+// import React from 'react'
+
+// export default function Province() {
+//     const body={
+//         userName :'',
+//         password:''
+//         }
+//         const config ={
+//             headers :{
+//                 'content-type':''
+//             }
+//         }
+//  const response= axios.post("" ,body,config)
+//  .then((res)=>  {return (res.data);})
+//     return (
+    
+//     <div>
+      
+//     </div>
+//   )
+// }
