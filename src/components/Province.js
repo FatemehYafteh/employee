1f50bb0a4e6537981bb6b35a2285a3d8b998e248
فاطمه یافته <fatemmehyafteh@gmail.com>
2024-12-07 15:33:25 +0300

    edite

--- src/components/Province.js
+++ src/components/Province.js
@@ -1,6 +1,8 @@
 // import axios from 'axios'
 // import React from 'react'
 
+import { useState } from "react"
+
 export default function Province() {
 //     const body={
 //         userName :'',
@@ -13,7 +15,12 @@ export default function Province() {
 //         }
 //  const response= axios.post("" ,body,config)
 //  .then((res)=>  {return (res.data);})
-const provincModelist=[
+const [provinceList,setProvinceList]=useState([
+    {
+        id: 13,
+        name: "خوزستان",
+        cities: null
+      },
 
 {
  id: 1,
@@ -64,12 +71,86 @@ name: "اردبيل",
     id: 10,
     name: "ایلام",
     province: null
-  }]
+  }])
+
+  const [cityModelList , setcityModelList]=useState([
+    {
+        id: 530,
+        name: "ميداود",
+        province: {
+          id: 13,
+          name: "خوزستان",
+          cities: null
+        }
+      },
+      {
+        id: 531,
+        name: "مينوشهر",
+        province: {
+          id: 13,
+          name: "خوزستان",
+          cities: null
+        }
+      },
+      {
+        id: 532,
+        name: "ويس",
+        province: {
+          id: 13,
+          name: "خوزستان",
+          cities: null
+        }
+      },
+      {
+        id: 533,
+        name: "هفتگل",
+        province: {
+          id: 13,
+          name: "خوزستان",
+          cities: null
+        }
+        },
+    
+        {
+            id: 161,
+            name: "ميمه",
+            province: {
+              id: 4,
+              name: "ايلام",
+              cities: null
+            }
+          },
+          {
+            id: 162,
+            name: "اسكو",
+            province: {
+              id: 5,
+              name: "آذربايجان شرقي",
+              cities: null
+            }
+          },
+          {
+            id: 163,
+            name: "اهر",
+            province: {
+              id: 5,
+              name: "آذربايجان شرقي",
+              cities: null
+            }
+          }
+  ])
+ const filterCityList=(pId)=>{
+    const newCityList=cityModelList.filter((item)=>item.province.id == pId);
+    setcityModelList (newCityList);
+  }
     return (
     
-    <div>
+    <div className="flex justify-center space-between">
       <select>
-       {provincModelist?.map((item)=>{return  <option value={item.id}>{item.name}</option>})}
+       {provinceList?.map((item)=>{return  <option key={item.id} onClick={(e)=>(console.log(e.target.value))} value={item.id}>{item.name}</option>})}
+      </select>
+      <select>
+       {cityModelList?.map((item)=>{return  <option key={item.id} onClick={(e)=>(console.log(e.target.value))} value={item.id}>{item.name + " استان " + item.province.name}</option>})}
       </select>
     </div>
   )
